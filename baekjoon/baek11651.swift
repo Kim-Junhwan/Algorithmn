d2370bc7dcd2e5f060144091c442b9fa65dd82eb
Kim-Junhwan <tazanboy1@naver.com>
2021-11-14 17:41:20 +0900

    baek11651

---
+++ baekjoon/baek11651.swift
@@ -0,0 +1,28 @@
+//
+//  baek11651.swift
+//  Algorithmn
+//
+//  Created by JunHwan Kim on 2021/11/14.
+//
+
+import Foundation
+
+var inputNum = Int(readLine()!)!
+
+var coordinate : [[Int]] = []
+
+for i in 0..<inputNum{
+    var coordinateInput = readLine()!.split(separator: " ").map(){Int($0)!}
+    coordinate.append(coordinateInput)
+}
+
+var sortedCoordinate = coordinate.sorted{
+    if $0[1] == $1[1]{
+        return $0[0] < $1[0]
+    }
+    return $0[1] < $1[1]
+}
+
+for item in 0..<sortedCoordinate.count{
+    print("\(sortedCoordinate[item][0]) \(sortedCoordinate[item][1])")
+}
