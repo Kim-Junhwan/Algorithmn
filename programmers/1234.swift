16e5d8d0e4a372e9edebc9fc7a183559f1834406
Kim-Junhwan <tazanboy1@naver.com>
2021-11-28 22:14:49 +0900

    baek15649

--- programmers/1234.swift
+++
@@ -1,7 +0,0 @@
-//
-//  progeammers 다리를 지나는 트럭.swift
-//  Algorithmn
-//
-//  Created by JunHwan Kim on 2021/10/29.
-//
-
---
+++ programmers/main.swift
@@ -0,0 +1,20 @@
+//
+//  progeammers 다리를 지나는 트럭.swift
+//  Algorithmn
+//
+//  Created by JunHwan Kim on 2021/10/29.
+//
+
+import Foundation
+
+func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+    var time = 0
+    var time : [Int] = []
+    var weight : [Int] = []
+    var waitTruck = truck_weights
+    
+    
+    return 0
+}
+
+
