599cbd206ee974994ac5f97309cba56dc1a455f6
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-01-14 23:05:39 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 236 ms, Memory: 183924 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.swift
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.swift
@@ -1,75 +1,75 @@
+//
+//  main.swift
+//  Algorithmn
+//
+//  Created by JunHwan Kim on 1/12/25.
+//
+
 import Foundation
 
-let nm = readLine()!.split(separator: " ").map{ Int($0)! }
-var map: [[Bool]] = []
-let nx = [0,1,0,-1]
-let ny = [-1,0,1,0]
-// 0은 true, 1은 false
-var visited: [[[Bool]]] = Array(repeating: Array(repeating: Array(repeating: false, count: nm[1]), count: nm[0]), count: 2)
-for _ in 0..<nm[0] {
-    let input = Array(readLine()!).map{ String($0) }
-    map.append(input.map{ Int($0)! == 0 })
-}
-
-struct User {
-    let canBreakBlock: Bool
-    let count: Int
+struct Horse {
+    let canBreakWall: Bool
     let x: Int
     let y: Int
+    let count: Int
 }
 
-func validate(x: Int, y: Int, canBreakBlock: Bool) -> Bool {
-    if x < 0 || x >= nm[1] || y < 0 || y >= nm[0] {
-        return false
-    }
-    if canBreakBlock {
-        return !visited[0][y][x] && map[y][x]
-    } else {
-        return !visited[1][y][x] && map[y][x]
-    }
-    
-}
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nm[0], m = nm[1]
+var map: [[Bool]] = []
+var visited: [[[Bool]]] = Array(repeating: Array(repeating: Array(repeating: false, count: m), count: n), count: 2)
+let dx = [1,0,-1,0]
+let dy = [0,-1,0,1]
 
-func checkUnvalidateIsBlock(x: Int, y: Int, canBreakBlock: Bool) -> Bool {
-    if x < 0 || x >= nm[1] || y < 0 || y >= nm[0] {
-        return false
-    }
-    if visited[canBreakBlock ? 0 : 1][y][x] {
-        return false
-    }
-    return !map[y][x]
+for _ in 0..<n {
+    let input = Array(readLine()!).map { $0 == "0" ? true : false }
+    map.append(input)
 }
 
 func solution() {
-    var queue: [User] = [.init(canBreakBlock: true, count: 1, x: 0, y: 0)]
+    var queue: [Horse] = [Horse(canBreakWall: true, x: 0, y: 0, count: 1)]
     visited[0][0][0] = true
     visited[1][0][0] = true
     var idx = 0
     while queue.count > idx {
-        let popUser = queue[idx]
+        let horse = queue[idx]
         idx += 1
-        if popUser.x == nm[1]-1 && popUser.y == nm[0]-1 {
-            print(popUser.count)
+        if horse.x == m-1 && horse.y == n-1 {
+            print(horse.count)
             return
         }
         for i in 0..<4 {
-            let nextX = popUser.x+nx[i]
-            let nextY = popUser.y+ny[i]
-            if validate(x: nextX, y: nextY, canBreakBlock: popUser.canBreakBlock) {
-                queue.append(.init(canBreakBlock: popUser.canBreakBlock, count: popUser.count+1, x: nextX, y: nextY))
-                visited[popUser.canBreakBlock ? 0 : 1][nextY][nextX] = true
-            } else {
-                if popUser.canBreakBlock {
-                    if checkUnvalidateIsBlock(x: nextX, y: nextY, canBreakBlock: popUser.canBreakBlock) {
-                        queue.append(.init(canBreakBlock: false, count: popUser.count+1, x: nextX, y: nextY))
+            let nextX = horse.x + dx[i]
+            let nextY = horse.y + dy[i]
+            if nextX < 0 || nextY < 0 || nextX >= m || nextY >= n || visited[horse.canBreakWall ? 0 : 1][nextY][nextX] {
+                continue
+            }
+            //벽을 만난 경우
+            if !map[nextY][nextX] {
+                //벽 부술수 있는 경우 append
+                if horse.canBreakWall {
+                    queue.append(Horse(canBreakWall: false, x: nextX, y: nextY, count: horse.count+1))
+                    visited[0][nextY][nextX] = true
+                    if nextX == m-1 && nextY == n-1 {
+                        print(horse.count+1)
+                        return
                     }
                 }
+            } else {
                 
+                if !visited[horse.canBreakWall ? 0 : 1][nextY][nextX] {
+                    queue.append(Horse(canBreakWall: horse.canBreakWall, x: nextX, y: nextY, count: horse.count+1))
+                    visited[horse.canBreakWall ? 0 : 1][nextY][nextX] = true
+                    if nextX == m-1 && nextY == n-1 {
+                        print(horse.count+1)
+                        return
+                    }
+                }
             }
+            
         }
     }
     print("-1")
-    return
 }
 
 solution()
