056c49ac06bd01d20c296ea3f7e59b111076e79c
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-06-11 22:36:22 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 48 ms, Memory: 91084 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.swift
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.swift
@@ -1,60 +1,88 @@
 import Foundation
 
-func solution() -> Int{
-    
-    let firstLine = readLine()!.split(separator: " ").map({Int($0)!})
-    let v = firstLine[0]
-    let e = firstLine[1]
-    
-    var parent = Array(0...v)
-    var graph: [[Int]] = []
-    var lines = 0
-    var result = 0
-    
-    // 부모 노드를 찾는 함수
-    func findParent(_ index: Int) -> Int{
-        
-        if parent[index] == index {
-            return index
-        } else {
-            parent[index] = findParent(parent[index])
-            return parent[index]
-        }
+final class FileIO {
+    private var buffer: [UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fileHandle.readToEnd()!) + [UInt8(0)]
     }
 
-    // 부모 노드를 합치는 함수
-    func unionParent(_ a: Int, _ b: Int) {
-        let num1 = findParent(a)
-        let num2 = findParent(b)
-        if a < b {
-            parent[num2] = num1
-        } else {
-            parent[num1] = num2
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var num = 0
+        var isNegative = false
+        var byte = read()
+        while byte == 10 || byte == 32 { byte = read() }
+        if byte == 45 { isNegative = true; byte = read() }
+        while byte >= 48, byte <= 57 {
+            num = num * 10 + Int(byte - 48)
+            byte = read()
         }
-        
+        return num * (isNegative ? -1 : 1)
     }
-    
-    
-    for _ in 0..<e {
-        graph.append(readLine()!.split(separator: " ").map({Int($0)!}))
-    }
-    
-    graph.sort { (a, b) -> Bool in
-        return a[2] < b[2]
-    }
-    
-    for index in 0..<graph.count {
-        if lines == v - 1 {
-            break
-        }
-        if findParent(graph[index][0]) != findParent(graph[index][1]) {
-            result += graph[index][2]
-            lines += 1
-            unionParent(graph[index][0], graph[index][1])
-        }
-    }
-    
-    return result
 }
 
-print(solution())
+struct Edge {
+    var start: Int
+    var end: Int
+    var weight: Int
+    var isPass: Bool = false
+}
+
+let file = FileIO()
+let v = file.readInt()
+let e = file.readInt()
+var edgeList: [Edge] = []
+var unionArr = Array(0..<v)
+
+for _ in 0..<e {
+    edgeList.append(Edge(start: file.readInt()-1, end: file.readInt()-1, weight: file.readInt()))
+}
+
+func solution() {
+    var answer = 0
+    var sortedEdgeList = edgeList.sorted { $0.weight < $1.weight }
+    var line = 0
+    for idx in 0..<sortedEdgeList.count {
+        if line == v-1 { break }
+        let edge = sortedEdgeList[idx]
+        let startParent = getParents(num: edge.start)
+        let endParent = getParents(num: edge.end)
+        
+        if startParent != endParent {
+            union(num1: edge.start, num2: edge.end)
+            answer += edge.weight
+            line += 1
+        }
+    }
+    
+    print(answer)
+}
+
+func getParents(num: Int) -> Int {
+    if unionArr[num] == num {
+        return num
+    }
+    let parents = getParents(num: unionArr[num])
+    unionArr[num] = parents
+    return parents
+}
+
+func union(num1: Int, num2: Int) {
+    let num1Parent = getParents(num: num1)
+    let num2Parent = getParents(num: num2)
+    
+    
+    if num1Parent > num2Parent {
+        unionArr[num1Parent] = num2Parent
+    } else if num1Parent < num2Parent {
+        unionArr[num2Parent] = num1Parent
+    }
+}
+
+solution()
