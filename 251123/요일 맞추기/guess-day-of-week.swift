afbdbb10d75b18f01d8cd3e4f72970997a2a99bc
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-11-23 01:59:43 +0900

    Title: 요일 맞추기, Time: 23ms, Memory: 3MB, Status: Runtime Error - Codetree

---
+++ 251123/요일 맞추기/guess-day-of-week.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+let input = readLine()!
+let values = input.split(separator: " ").map { Int(String($0))! }
+let m1 = values[0]
+let d1 = values[1]
+let m2 = values[2]
+let d2 = values[3]
+
+let daysArr = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+solution()
+
+func getRemainDays() -> Int {
+    var currentMonth = m1
+    var currentDay = d1
+    var isBefore: Bool = false
+    if m1 > m2 || d1 > d2 {
+        isBefore = true
+    }
+
+    var days = 0
+
+    while true {
+        if currentMonth == m2 && currentDay == d2 {
+            return days
+        }
+        var nextMonth: Int = currentMonth
+        var nextDays: Int = currentDay
+        if isBefore {
+            nextDays = currentDay - 1
+            if nextDays <= 0 {
+                nextMonth -= 1
+                nextDays = daysArr[nextMonth]
+            }
+        } else {
+            nextDays = currentDay + 1
+            if nextDays > daysArr[currentMonth] {
+                nextMonth = currentMonth+1
+                nextDays = 1
+            }
+        }
+        currentMonth = nextMonth
+        currentDay = nextDays
+
+        days += 1
+    }
+}
+
+func getdw(elapse: Int) -> String {
+    let dwArr = ["Mon", "Tue", "Wed", "Thu" ,"Fri" , "Sat" , "Sun"]
+    
+    if elapse < 0 {
+        return dwArr[dwArr.count+elapse]
+    } else {
+        return dwArr[elapse]
+    }
+}
+
+func solution() {
+    let elapseDay = getRemainDays()
+    let remainDay = elapseDay%7
+    var isBefore: Bool = false
+    if m1 > m2 || d1 > d2 {
+        isBefore = true
+    }
+
+    print(getdw(elapse: isBefore ? -remainDay : remainDay))
+}
