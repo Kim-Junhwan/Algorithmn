ca5ea971eb0866141910b27847f38c4a94aa0234
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-11-23 00:57:20 +0900

    Title: DateTime to DateTime, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251123/DateTime to DateTime/datetime-to-datetime.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+let inputLine = readLine()!
+let numbers = inputLine.split(separator: " ").map { Int(String($0))! }
+let a = numbers[0]
+let b = numbers[1]
+let c = numbers[2]
+
+
+func solution() {
+var minutes = -(11*60+11)
+var currentDay = 11
+while true {
+    if currentDay == a {
+        minutes += ((b*60) + c)
+        break
+    }
+
+    currentDay += 1
+
+    minutes += 24 * 60
+}
+
+print(minutes > 0 : minutes ? -1)
+}
