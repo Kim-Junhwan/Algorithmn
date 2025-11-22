fbfd41ab426441833d378b2a4c492ce681519f03
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-11-23 00:44:28 +0900

    Title: Date to Date, Time: 11ms, Memory: 1MB, Status: Passed - Codetree

--- 251123/Date to Date/date-to-date.swift
+++ 251123/Date to Date/date-to-date.swift
@@ -5,9 +5,10 @@ let m2 = input[2]
 let d2 = input[3]
 
 let month = [0,31,28,31,30,31,30,31,31,30,31,30,31]
-var answer = 0
+var answer = 1
 var currentMonth = m1
 var currentDay = d1
+
 while true {
 
     if currentMonth == m2 && currentDay == d2 {
@@ -15,12 +16,14 @@ while true {
     }
 
     let nextDay = currentDay + 1
-
+    
     let maxDay = month[currentMonth]
 
     if maxDay < nextDay {
         currentMonth += 1
         currentDay = 1
+    } else {
+    currentDay = nextDay
     }
     answer += 1
 }
