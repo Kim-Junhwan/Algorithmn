544d5a45908b33c36cb9ee798ac22ca36f1667ea
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-11-23 00:41:11 +0900

    Title: Date to Date, Time: 11ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 251123/Date to Date/date-to-date.swift
@@ -0,0 +1,28 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let m1 = input[0]
+let d1 = input[1]
+let m2 = input[2]
+let d2 = input[3]
+
+let month = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+var answer = 0
+var currentMonth = m1
+var currentDay = d1
+while true {
+
+    if currentMonth == m2 && currentDay == d2 {
+        break
+    }
+
+    let nextDay = currentDay + 1
+
+    let maxDay = month[currentMonth]
+
+    if maxDay < nextDay {
+        currentMonth += 1
+        currentDay = 1
+    }
+    answer += 1
+}
+
+print(answer)
