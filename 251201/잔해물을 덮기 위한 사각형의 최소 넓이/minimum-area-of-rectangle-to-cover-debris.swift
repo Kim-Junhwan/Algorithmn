f2662c4209a59e73cba922f97b47bb4e99daa815
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-12-01 21:44:22 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 24ms, Memory: 5MB, Status: Runtime Error - Codetree

---
+++ 251201/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+let rect1Input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let rect2Input = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+let rect1X1 = rect1Input[0]
+let rect1Y1 = rect1Input[1]
+let rect1X2 = rect1Input[2]
+let rect1Y2 = rect1Input[3]
+
+let rect2X1 = rect2Input[0]
+let rect2Y1 = rect2Input[1]
+let rect2X2 = rect2Input[2]
+let rect2Y2 = rect2Input[3]
+let offsetX = min(rect1X1, rect2X1)
+let offsetY = min(rect1Y1, rect2Y1)
+
+var map: [[Bool]] = Array(repeating: Array(repeating: false, count: 2000), count: 2000)
+
+for i in (rect1Y1-offsetY)..<(rect1Y2-offsetY) {
+    for j in (rect1X1-offsetX)..<(rect1X2-offsetX) {
+        map[i][j] = true
+    }
+}
+
+for i in (rect2Y1-offsetY)..<(rect2Y2-offsetY) {
+    for j in (rect2X1-offsetX)..<(rect2X2-offsetX) {
+        map[i][j] = false
+    }
+}
+
+var minX = Int.max
+var maxX = -Int.max
+var minY = Int.max
+var maxY = -Int.max
+
+for i in (rect1Y1-offsetY)..<(rect1Y2-offsetY) {
+    for j in (rect1X1-offsetX)..<(rect1X2-offsetX) {
+        if map[i][j] {
+            minX = min(minX, j)
+            maxX = max(maxX, j)
+            minY = min(minY, i)
+            maxY = max(maxY, i)
+        }
+    }
+}
+
+print((maxX-minX+1) * (maxY-minY+1))
