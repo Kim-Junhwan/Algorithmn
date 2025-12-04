cb6e47c94e19897569ca0a64d959b7a98ddc3312
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-12-04 21:58:38 +0900

    Title: 연속되는 수 4, Time: 13ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 251204/연속되는 수 4/continuous-number4.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var numbers = [Int]()
+for _ in 0..<n {
+    numbers.append(Int(readLine()!)!)
+}
+
+var maxCount = 0
+var count = 1
+for i in 1..<n {
+    if numbers[i] > numbers[i-1] {
+        count += 1
+    } else {
+        count = 1
+    }
+    maxCount = max(maxCount, count)
+}
+
+print(maxCount)
