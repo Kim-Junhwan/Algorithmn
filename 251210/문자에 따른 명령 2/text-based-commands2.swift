c260312e9ed05ac6b09b00b44faaeda776831ede
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-12-10 20:40:10 +0900

    Title: 문자에 따른 명령 2, Time: 16ms, Memory: 5MB, Status: Wrong Answer - Codetree

---
+++ 251210/문자에 따른 명령 2/text-based-commands2.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+let commands = Array(readLine()!)
+
+var coordi = (0,0)
+var direction = 0
+
+let dx = [0,1,0,-1]
+let dy = [-1,0,1,0]
+
+for command in commands {
+    if command == "L" {
+        direction = (direction+3)%4 
+    } else if command == "R" {
+        direction = (direction + 1)%4
+    } else {
+        coordi = (coordi.0+dx[direction], coordi.1+dy[direction])
+    }
+}
+
+print("\(coordi.0) \(coordi.1)")
