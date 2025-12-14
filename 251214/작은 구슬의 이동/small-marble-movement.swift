4e3ea08c67dab46c9904dc2f29bf66e64031feb8
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-12-14 23:47:14 +0900

    Title: 작은 구슬의 이동, Time: 17ms, Memory: 7MB, Status: Wrong Answer - Codetree

---
+++ 251214/작은 구슬의 이동/small-marble-movement.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+let firstLine = readLine()!.split(separator: " ").map { Int(String($0))! }
+let n = firstLine[0]
+let t = firstLine[1]
+
+let secondLine = readLine()!.split(separator: " ")
+var r = Int(String(secondLine[0]))!
+var c = Int(String(secondLine[1]))!
+var d = String(secondLine[2])
+
+var currentLocation = (r-1, c-1)
+var currentDirection = 0
+
+if d == "U" {
+    currentDirection = 1
+} else if d == "D" {
+    currentDirection = 3
+} else if d == "R" {
+    currentDirection = 0
+} else {
+    currentDirection = 2
+}
+
+let dx = [1,0,-1,0]
+let dy = [0,1,0,-1]
+var answer = 0
+
+for _ in 0..<t {
+    let nextX = currentLocation.1 + dx[currentDirection]
+    let nextY = currentLocation.0 + dy[currentDirection]
+
+    if nextX < 0 || nextY >= n || nextY < 0 || nextY >= n {
+        currentDirection = (currentDirection + 2)%4
+        answer += 1
+        continue
+    }
+
+    currentLocation = (nextY, nextX)
+    answer += 1
+}
+
+print("\(currentLocation.0+1) \(currentLocation.1+1)")
