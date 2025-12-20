5e89d8e63640a90e4a5ca8ef173d1be3c1400555
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-12-20 17:54:18 +0900

    Title: 이상한 진수 2, Time: 18ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 251220/이상한 진수 2/awkward-digits-2.swift
+++ 251220/이상한 진수 2/awkward-digits-2.swift
@@ -10,23 +10,28 @@ func pow(_ x: Int, _ y: Int) -> Int {
     return sum
 }
 
+func twoToTen(_ two: [Character]) -> Int {
+    var sum = 0
+    for j in 0..<two.count {
+            if two[j] == "1" {
+            sum += pow(2, two.count-1-j)
+            }
+        }
+    return sum
+}
+
 func solution() {
 for i in 0..<binary.count {
     if binary[i] == "0" {
         var cpBn = binary
         cpBn[i] = "1"
-        var sum = 0
-
-
-        for j in 0..<binary.count {
-            if cpBn[j] == "1" {
-            sum += pow(2, binary.count-1-j)
-            }
-        }
-        print(sum)
+        print(twoToTen(cpBn))
         return
     }
 }
+ print(twoToTen(binary))
 }
+
+
 
 solution()
