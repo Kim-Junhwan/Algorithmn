f907dc00c6d617734864b136d0b10f278d0daff4
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2025-12-27 14:53:07 +0900

    Title: Carry 피하기 2, Time: 13ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 251227/Carry 피하기 2/escaping-carry-2.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var numbers = [Int]()
+for _ in 0..<n {
+    let number = Int(readLine()!)!
+    numbers.append(number)
+}
+
+func digitsMath(_ number: Int) -> [Int] {
+    var n = abs(number)
+    var result: [Int] = []
+
+    while n > 0 {
+        result.append(n % 10)
+        n /= 10
+    }
+
+    return result
+}
+
+func isCarry(_ a: Int, _ b: Int, _ c: Int) -> Bool {
+    var ten = 10
+    let maxNum = max(max(a,b),c)
+
+    let aArr = digitsMath(a)
+    let bArr = digitsMath(b)
+    let cArr = digitsMath(c)
+
+    let maxLength = max(max(aArr.count, bArr.count), cArr.count)
+
+    for i in 0..<maxLength {
+        let aNum = aArr.count > i ? aArr[i] : 0
+        let bNum = bArr.count > i ? bArr[i] : 0
+        let cNum = cArr.count > i ? cArr[i] : 0
+
+        if aNum + bNum + cNum >= 10 {
+            return false
+        }
+    }
+
+    return true
+}
+
+var answer = Int.min
+
+for i in 0..<numbers.count-2 {
+    for j in i+1..<numbers.count-1 {
+        for z in j+1..<numbers.count {
+            let a = numbers[i]
+            let b = numbers[j]
+            let c = numbers[z]
+
+            if isCarry(a,b,c) {
+                answer = max(answer, a+b+c)
+            }
+        }
+    }
+}
+
+print(answer)
