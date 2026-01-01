1752711438ce2a35d90ac3f30c18d9baaa73583a
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2026-01-01 13:41:28 +0900

    Title: 아름다운 수열 2, Time: 1091ms, Memory: 317MB, Status: Time Limit Exceed - Codetree

---
+++ 260101/아름다운 수열 2/beautiful-sequence-2.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nm[0]
+let m = nm[1]
+
+let a = readLine()!.split(separator: " ").map { Int($0)! }
+let b = readLine()!.split(separator: " ").map { Int($0)! }
+
+func getSoon() -> [[Int]] {
+    var setArr = Set<[Int]>()
+    var visited: [Bool] = Array(repeating: false, count: m)
+    func a(start: [Int], depth: Int) {
+        if depth == b.count {
+            setArr.insert(start)
+            return
+        }
+        for i in 0..<b.count {
+            if visited[i] { continue }
+            var next = start
+            next.append(b[i])
+            visited[i] = true
+            a(start: next, depth: depth+1)
+            visited[i] = false
+        }
+    }
+    for num in 0..<b.count {
+        visited[num] = true
+        a(start:[b[num]], depth: 1)
+        visited[num] = false
+    }
+    return Array(setArr)
+}
+
+let mSoon = getSoon()
+var answer = 0
+for i in 0..<n-m+1 {
+    if mSoon.contains(Array(a[i..<i+m])) {
+        answer += 1
+    }
+}
+
+print(answer)
