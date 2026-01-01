962b73d780e4146244a8f5deda9ab9da52d2320c
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2026-01-01 14:14:32 +0900

    Title: 아름다운 수열 2, Time: 22ms, Memory: 1MB, Status: Runtime Error - Codetree

--- 260101/아름다운 수열 2/beautiful-sequence-2.swift
+++ 260101/아름다운 수열 2/beautiful-sequence-2.swift
@@ -7,43 +7,31 @@ let m = nm[1]
 let a = readLine()!.split(separator: " ").map { Int($0)! }
 let b = readLine()!.split(separator: " ").map { Int($0)! }
 
-func getSoon() -> [[Int]] {
-    var setArr = Set<[Int]>()
-    var visited: [Bool] = Array(repeating: false, count: m)
-    func a(start: [Int], depth: Int) {
-        if depth == b.count {
-            setArr.insert(start)
-            return
-        }
-        for i in 0..<b.count {
-            if visited[i] { continue }
-            var next = start
-            next.append(b[i])
-            visited[i] = true
-            a(start: next, depth: depth+1)
-            visited[i] = false
-        }
+var bDict: [Int: Int] = [:]
+
+for i in b {
+    if bDict[i] == nil {
+        bDict[i] = 1
+    } else {
+        bDict[i]! += 1
     }
-    for num in 0..<b.count {
-        visited[num] = true
-        a(start:[b[num]], depth: 1)
-        visited[num] = false
-    }
-    return Array(setArr)
 }
 
-if n < m {
-    print(0)
-} else if n == m {
-    print(a == b ? 1 : 0)
-} else {
-let mSoon = getSoon()
 var answer = 0
+
 for i in 0..<n-m+1 {
-    if mSoon.contains(Array(a[i..<i+m])) {
+    var aDict: [Int: Int] = [:]
+    for j in Array(a[i..<i+m]) {
+        if aDict[j] == nil {
+            aDict[j] = 1
+        } else {
+            aDict[j]! += 1
+        }
+    }
+
+    if aDict == bDict {
         answer += 1
     }
 }
 
 print(answer)
-}
