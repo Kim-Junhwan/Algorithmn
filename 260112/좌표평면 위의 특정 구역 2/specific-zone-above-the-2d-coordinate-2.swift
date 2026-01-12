3ee96a38ee6a490b01560b46e086054b6e76ead3
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2026-01-12 21:34:20 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 14ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260112/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var points: [(x: Int, y: Int)] = []
+for _ in 0..<n {
+    let line = readLine()!
+    let parts = line.split(separator: " ").map { Int($0)! }
+    points.append((x: parts[0], y: parts[1]))
+}
+var answer = Int.max
+
+for ignoreCoordi in 0..<points.count {
+    var maxX: Int = Int.min
+    var maxY: Int = Int.min
+    var minX: Int = Int.max
+    var minY: Int = Int.max
+
+    for i in 0..<points.count {
+        if i == ignoreCoordi { continue }
+        maxX = max(maxX, points[i].x)
+        maxY = max(maxY, points[i].y)
+        minX = min(minX, points[i].x)
+        minY = min(minX, points[i].y)
+    }
+    answer = min(answer, (maxX - minX) * (maxY - minY))
+}
+print(answer)
