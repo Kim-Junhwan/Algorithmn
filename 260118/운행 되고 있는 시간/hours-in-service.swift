26396976582203da3cc81672f066ef0a5b0148a3
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2026-01-18 01:03:01 +0900

    Title: 운행 되고 있는 시간, Time: 13ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260118/운행 되고 있는 시간/hours-in-service.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+var leftTimes = [Int]()
+var rightTimes = [Int]()
+
+for _ in 0..<n {
+    let parts = readLine()!.split(separator: " ").map { Int($0)! }
+    leftTimes.append(parts[0])
+    rightTimes.append(parts[1])
+}
+
+var workingTime: [Int] = Array(repeating: 0, count: 1001)
+
+for i in 0..<n {
+    for j in leftTimes[i]...rightTimes[i] {
+        workingTime[j] += 1
+    }
+}
+var answer = 0
+for i in 0..<n {
+    for j in leftTimes[i]...rightTimes[i] {
+        workingTime[j] -= 1
+    }
+    answer = max(answer, workingTime.filter{$0 > 0}.count)
+}
+
+print(answer)
