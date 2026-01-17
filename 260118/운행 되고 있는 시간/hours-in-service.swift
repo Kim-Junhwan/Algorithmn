72eb082402b9532c7662b5401d177bf655c23e90
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2026-01-18 01:15:46 +0900

    Title: 운행 되고 있는 시간, Time: 14ms, Memory: 1MB, Status: Passed - Codetree

--- 260118/운행 되고 있는 시간/hours-in-service.swift
+++ 260118/운행 되고 있는 시간/hours-in-service.swift
@@ -11,20 +11,20 @@ for _ in 0..<n {
     rightTimes.append(parts[1])
 }
 
-var workingTime: [Int] = Array(repeating: 0, count: 1000)
+var workingTime: [Int] = Array(repeating: 0, count: 1001)
 
 for i in 0..<n {
-    for j in leftTimes[i]...rightTimes[i] {
+    for j in leftTimes[i]...rightTimes[i]-1 {
         workingTime[j] += 1
     }
 }
 var answer = 0
 for i in 0..<n {
     var cpWork = workingTime
-    for j in leftTimes[i]...rightTimes[i] {
+    for j in leftTimes[i]...rightTimes[i]-1 {
         cpWork[j] -= 1
     }
-    answer = max(answer, cpWork.filter{$0 > 0}.count-2)
+    answer = max(answer, cpWork.filter{$0 > 0}.count)
 }
 
 print(answer)
