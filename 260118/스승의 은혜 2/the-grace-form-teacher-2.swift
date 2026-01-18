a86b422371ed15d746bb65531ef6b3ce3ddace37
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2026-01-18 21:11:38 +0900

    Title: 스승의 은혜 2, Time: 20ms, Memory: 1MB, Status: Runtime Error - Codetree

---
+++ 260118/스승의 은혜 2/the-grace-form-teacher-2.swift
@@ -0,0 +1,23 @@
+let firstLine = readLine()!.split(separator: " ")
+let n = Int(firstLine[0])!
+let b = Int(firstLine[1])!
+var prices = [Int]()
+for _ in 0..<n {
+    let price = Int(readLine()!)!
+    prices.append(price)
+}
+
+prices = prices.sorted()
+var priceSum: [Int] = []
+var sum = 0 
+for i in 0..<prices.count {
+    sum += prices[i]
+    priceSum.append(sum)
+}
+var answer = 0
+for i in 0..<prices.count {
+    let salePrice = prices[i]/2
+    answer = max(answer, priceSum.firstIndex { $0 > b+salePrice }!)
+}
+
+print(answer)
