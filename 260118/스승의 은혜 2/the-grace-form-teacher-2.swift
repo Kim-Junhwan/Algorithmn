c6b7295921d39b0fbc633ec38da51c76ff7d6404
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2026-01-18 21:31:35 +0900

    Title: 스승의 은혜 2, Time: 14ms, Memory: 1MB, Status: Passed - Codetree

--- 260118/스승의 은혜 2/the-grace-form-teacher-2.swift
+++ 260118/스승의 은혜 2/the-grace-form-teacher-2.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 let firstLine = readLine()!.split(separator: " ")
 let n = Int(firstLine[0])!
 let b = Int(firstLine[1])!
@@ -9,7 +11,7 @@ for _ in 0..<n {
 
 prices = prices.sorted()
 var priceSum: [Int] = []
-var sum = 0 
+var sum = 0
 for i in 0..<prices.count {
     sum += prices[i]
     priceSum.append(sum)
@@ -17,7 +19,10 @@ for i in 0..<prices.count {
 var answer = 0
 for i in 0..<prices.count {
     let salePrice = prices[i]/2
-    answer = max(answer, priceSum.firstIndex { $0 > b+salePrice } ?? n)
+    answer = max(answer, priceSum.indices.firstIndex { idx in
+        let threshold = (idx >= i) ? (b + salePrice) : b
+            return priceSum[idx] > threshold
+    } ?? n)
 }
 
 print(answer)
