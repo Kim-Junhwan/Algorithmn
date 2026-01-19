e62ce92f7a46d768b352ff982ef538fa05693771
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2026-01-19 23:22:16 +0900

    Title: 상해버린 치즈, Time: 16ms, Memory: 4MB, Status: Wrong Answer - Codetree

---
+++ 260119/상해버린 치즈/rotten-cheese.swift
@@ -0,0 +1,60 @@
+import Foundation
+
+let firstLine = readLine()!
+let tokens = firstLine.split(separator: " ").map { Int($0)! }
+let n = tokens[0]
+let m = tokens[1]
+let d = tokens[2]
+let s = tokens[3]
+
+var info1: [[[Int]]] = Array(repeating: Array(repeating: [], count: m), count: n)
+for _ in 0..<d {
+    let line = readLine()!
+    let parts = line.split(separator: " ").map { Int($0)! }
+    let person = parts[0]-1
+    let cheese = parts[1]-1
+    let time = parts[2]
+    info1[person][cheese].append(time)
+}
+
+var info2: [(p: Int, t: Int)] = []
+for _ in 0..<s {
+    let line = readLine()!
+    let parts = line.split(separator: " ").map { Int($0)! }
+    let person = parts[0]-1
+    let time = parts[1]
+    info2.append((p: person, t: time))
+}
+
+var dropCheese: [Bool] = Array(repeating: false, count: m)
+
+// t시간 이전에 p사람이 cheese를 먹은 지 확인하는 함수
+func checkIsEatCheese(_ p: Int, _ t: Int, _ cheese: Int) -> Bool {
+    let eatTimeArr = info1[p][cheese]
+    for eatTime in eatTimeArr {
+        if eatTime < t {
+            return true
+        }
+    }
+    return false
+}
+
+for cheese in 0..<m {
+    
+    for sickInfo in info2 {
+        
+        // 아픈 경우 아픈 시간 이전에 치즈 먹었는지 체크
+        if checkIsEatCheese(sickInfo.p, sickInfo.t, cheese) {
+            dropCheese[cheese] = true
+        } else {
+            dropCheese[cheese] = false
+        }
+    }
+}
+var answerPeople: [Int] = []
+for i in dropCheese.indices.filter { dropCheese[$0] } {
+    let cheeseEatPeople = info1.indices.filter { !info1[$0][i].isEmpty }.flatMap{$0}
+    answerPeople.append(contentsOf: cheeseEatPeople)
+}
+let set = Set(answerPeople)
+print(set.count)
