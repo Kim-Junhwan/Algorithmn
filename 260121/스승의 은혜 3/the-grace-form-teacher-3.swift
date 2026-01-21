4d6781373a3239a244dc1d291cd5e9848148f87b
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2026-01-21 23:02:20 +0900

    Title: 스승의 은혜 3, Time: 19ms, Memory: 4MB, Status: Wrong Answer - Codetree

--- 260121/스승의 은혜 3/the-grace-form-teacher-3.swift
+++ 260121/스승의 은혜 3/the-grace-form-teacher-3.swift
@@ -3,30 +3,30 @@ import Foundation
 let firstLine = readLine()!.components(separatedBy: " ")
 let n = Int(firstLine[0])!
 let b = Int(firstLine[1])!
-
-var prices = [Int]()
-var quick = [Int]()
+// 선물 , 배송비
+var studentInfos: [(Int, Int)] = []
 
 for _ in 0..<n {
-    let line = readLine()!.components(separatedBy: " ")
-    prices.append(Int(line[0])!)
-    quick.append(Int(line[1])!)
+    let line = readLine()!.components(separatedBy: " ").map{Int($0)!}
+    studentInfos.append((line[0],line[1]))
 }
 
-prices = prices.sorted()
+studentInfos = studentInfos.sorted { ($0.0+$0.1) < ($1.0+$1.1) }
 var priceSum: [Int] = []
 var sum = 0
-for i in 0..<prices.count {
-    sum += (prices[i]+quick[i])
+for i in 0..<studentInfos.count {
+    sum += (studentInfos[i].0 + studentInfos[i].1)
     priceSum.append(sum)
 }
 var answer = 0
-for i in 0..<prices.count {
-    let salePrice = prices[i]/2
+for i in 0..<studentInfos.count {
+    let salePrice = studentInfos[i].0/2
+    let quickCost = studentInfos[i].1
     answer = max(answer, priceSum.indices.firstIndex { idx in
-        let threshold = (idx >= i) ? (b + salePrice + quick[i]) : b + quick[i]
+        let threshold = (idx >= i) ? (b + salePrice + quickCost) : b + quickCost
             return priceSum[idx] > threshold
     } ?? n)
 }
 
 print(answer)
+
