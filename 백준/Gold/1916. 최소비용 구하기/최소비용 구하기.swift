2bd14d5aea12120d719a466a37b8977e4d4f67ef
Kim junhwan <58679737+Kim-Junhwan@users.noreply.github.com>
2026-03-23 21:26:39 +0900

    [Gold V] Title: 최소비용 구하기, Time: 176 ms, Memory: 81780 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.swift
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.swift
@@ -1,120 +1,53 @@
 import Foundation
 
-struct City: Comparable {
-    static func < (lhs: City, rhs: City) -> Bool {
-        return lhs.count < rhs.count
-    }
-    
-    let num: Int
-    let count: Int
-}
-
-struct PriorityQueue {
-    var heap: Array<City> = [City(num: 0, count: 0)]
-    var count: Int  {
-        return heap.count
-    }
-    
-    var isEmpty: Bool {
-        return heap.count <= 1
-    }
-    
-    mutating func insert(element: City) {
-        heap.append(element)
-
-        var currentIndex = heap.count - 1
-        var parentsIndex = currentIndex / 2
-        
-        while canSwap() {
-            heap.swapAt(currentIndex, parentsIndex)
-            currentIndex = parentsIndex
-            parentsIndex = currentIndex / 2
-        }
-        
-        func canSwap() -> Bool {
-            guard currentIndex > 1 else { return false }
-            
-            return heap[currentIndex] < heap[parentsIndex] ? true : false
-        }
-    }
-
-    mutating func pop() -> City? {
-        heap.swapAt(1, heap.count - 1)
-        let out = heap.removeLast()
-        
-        var parentsIndex = 1
-        var leftIndex = parentsIndex * 2
-        var rightIndex = leftIndex + 1
-        var childIndex = 0
-        
-        while parentsIndex < heap.count {
-            
-            // 왼쪽노드만 있을 경우
-            if rightIndex >= heap.count && leftIndex < heap.count {
-                childIndex = leftIndex
-            }
-            // 둘 다 없을 경우
-            else if rightIndex >= heap.count && leftIndex >= heap.count {
-                return out
-            }
-            // 둘 다 있을 경우
-            else if rightIndex < heap.count && leftIndex < heap.count {
-                childIndex = heap[leftIndex] > heap[rightIndex] ? rightIndex : leftIndex
-            }
-            
-            if heap[childIndex] < heap[parentsIndex] {
-                heap.swapAt(childIndex, parentsIndex)
-                
-                parentsIndex = childIndex
-                leftIndex = parentsIndex * 2
-                rightIndex = leftIndex + 1
-            } else {
-                return out
-            }
-        }
-
-        return out
-    }
-}
-
 let n = Int(readLine()!)!
 let m = Int(readLine()!)!
-
-var relationArr: [[(Int, Int)]] = Array(repeating: [], count: n+1)
-var distanceArr = Array(repeating: 100000000, count: n+1)
-var visited = Array(repeating: false, count: n+1)
-
+// (도착도시 , 비용)
+var relation: [[Int:Int]] = Array(repeating: [:], count: n+1)
 for _ in 0..<m {
     let input = readLine()!.split(separator: " ").map{Int($0)!}
-    relationArr[input[0]].append((input[1], input[2]))
+    if relation[input[0]][input[1]] == nil {
+        relation[input[0]][input[1]] = input[2]
+    } else if let dist = relation[input[0]][input[1]], dist > input[2] {
+            relation[input[0]][input[1]] = input[2]
+    }
+}
+let ab = readLine()!.split(separator: " ").map{Int($0)!}
+
+var dist: [Int] = Array(repeating: Int.max, count: n+1)
+var visited: [Bool] = Array(repeating: false, count: n+1)
+
+func findMinNode() -> Int {
+    var minDist: Int = Int.max
+    var minNode: Int = 0
+    
+    for i in 1..<dist.count {
+        if dist[i] < minDist && !visited[i] {
+            minDist = dist[i]
+            minNode = i
+        }
+    }
+    return minNode
 }
 
-let startEnd = readLine()!.split(separator: " ").map{Int($0)!}
-let start = startEnd[0]
-let end = startEnd[1]
-
-func daiksstra(start: Int) {
-    var queue = PriorityQueue()
-    queue.insert(element: City(num: start, count: 0))
-    distanceArr[start] = 0
+func daikstra(start: Int) {
+    for i in relation[start] {
+        dist[i.0] = i.1
+    }
+    dist[start] = 0
+    visited[start] = true
     
-    while !queue.isEmpty {
-        let pop = queue.pop()!
-        
-        if visited[pop.num] {
-            continue
-        }
-        visited[pop.num] = true
-        
-        for nextCity in relationArr[pop.num] {
-            
-            if (distanceArr[nextCity.0] > distanceArr[pop.num] + nextCity.1) {
-                distanceArr[nextCity.0] = distanceArr[pop.num] + nextCity.1
-                queue.insert(element: City(num: nextCity.0, count: distanceArr[nextCity.0]))
+    for _ in 0..<n-1 {
+        let current = findMinNode()
+        visited[current] = true
+        for (next, weight) in relation[current] {
+            if visited[next] { continue }
+            if dist[current] + weight < dist[next] {
+                dist[next] = dist[current] + weight
             }
         }
     }
-    print(distanceArr[end])
 }
+daikstra(start: ab[0])
 
-daiksstra(start: start)
+print(dist[ab[1]])
